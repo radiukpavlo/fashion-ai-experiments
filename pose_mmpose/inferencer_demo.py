6e0f8b414008fc90b9e04e7033907daeac6c131f
Pavlo Radiuk <radiukpavlo@gmail.com>
2023-07-21 14:39:33 +0300

    Update inferencer_demo.py

--- pose_mmpose/inferencer_demo.py
+++ pose_mmpose/inferencer_demo.py
@@ -155,9 +155,9 @@ def parse_args():
     for init_kw in init_kws:
         init_args[init_kw] = call_args.pop(init_kw)
 
-    diaplay_alias = call_args.pop('show_alias')
+    display_alias = call_args.pop('show_alias')
 
-    return init_args, call_args, diaplay_alias
+    return init_args, call_args, display_alias
 
 
 def display_model_aliases(model_aliases: Dict[str, str]) -> None:
@@ -171,10 +171,10 @@ def display_model_aliases(model_aliases: Dict[str, str]) -> None:
 
 
 def main():
-    init_args, call_args, diaplay_alias = parse_args()
-    if diaplay_alias:
-        model_alises = get_model_aliases(init_args['scope'])
-        display_model_aliases(model_alises)
+    init_args, call_args, display_alias = parse_args()
+    if display_alias:
+        model_aliases = get_model_aliases(init_args['scope'])
+        display_model_aliases(model_aliases)
     else:
         inferencer = MMPoseInferencer(**init_args)
         for _ in inferencer(**call_args):
